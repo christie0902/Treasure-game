155955900b63eac2e59483f9236b80ea4b0edbbb
Christie Pham <115462474+christie0902@users.noreply.github.com>
2024-01-25 22:26:08 +0100

    add restart button

--- js/game.js
+++ js/game.js
@@ -16,6 +16,54 @@ const treasurePosition = {x:400, y:0};
 const ghostPosition = {x:600, y:200};
 const map = document.querySelector('.map');
 
+// Win Screen
+const winCondition = () => {
+if (heroPosition.x === treasurePosition.x && heroPosition.y === treasurePosition.y) {
+    map.style.opacity="0.5";
+    const winMessage = document.createElement('h1');
+    map.appendChild(winMessage);
+    winMessage.innerText="YOU WIN!";
+    winMessage.style.color= "green";
+    winMessage.style.backgroundColor = "white";
+    winMessage.style.fontFamily = "sans-serif";
+    winMessage.style.position = "absolute";
+    winMessage.style.top= "260px";
+    winMessage.style.left= "300px";
+    createButton(winMessage);
+}
+}
+
+// Lost Screen
+const lostCondtion = () => {
+    if (heroPosition.x === ghostPosition.x && heroPosition.y === ghostPosition.y) {
+        map.style.opacity="0.5";
+        const lostMessage = document.createElement('h1');
+        map.appendChild(lostMessage);
+        lostMessage.innerText="YOU LOSE!";
+        lostMessage.style.color= "red";
+        lostMessage.style.backgroundColor = "white";
+        lostMessage.style.fontFamily = "sans-serif";
+        lostMessage.style.position = "absolute";
+        lostMessage.style.top= "260px";
+        lostMessage.style.left= "300px";
+        createButton(lostMessage);
+    }
+}
+
+// Create Restart Button
+const createButton = (msg) => {
+    const button = document.createElement('button');
+    msg.appendChild(button);
+    msg.style.display = "flex";
+    button.innerText="RESTART";
+    button.style.background="white";
+    button.style.fontWeight="bold";
+    button.addEventListener('click', (event) => {
+        location.reload();
+    })
+}
+
+
 document.addEventListener("keydown", (event) => {
     switch (event.keyCode) {
         case 39:
@@ -46,65 +94,41 @@ document.addEventListener("keydown", (event) => {
     hero.style.top = heroPosition.y + "px";
     hero.style.left = heroPosition.x + "px";
     
-    // Winning screen
-    if (heroPosition.x === treasurePosition.x && heroPosition.y === treasurePosition.y) {
-        map.style.opacity="0.5";
-        const winMessage = document.createElement('h1');
-        map.appendChild(winMessage);
-        winMessage.innerText="YOU WIN!";
-        winMessage.style.color= "green";
-        winMessage.style.backgroundColor = "white";
-        winMessage.style.fontFamily = "sans-serif";
-        winMessage.style.position = "absolute";
-        winMessage.style.top= "260px";
-        winMessage.style.left= "300px";
-    }
-
-    // Losing screen
-    if (heroPosition.x === ghostPosition.x && heroPosition.y === ghostPosition.y) {
-        map.style.opacity="0.5";
-        const lostMessage = document.createElement('h1');
-        map.appendChild(lostMessage);
-        lostMessage.innerText="YOU LOSE!";
-        lostMessage.style.color= "red";
-        lostMessage.style.backgroundColor = "white";
-        lostMessage.style.fontFamily = "sans-serif";
-        lostMessage.style.position = "absolute";
-        lostMessage.style.top= "260px";
-        lostMessage.style.left= "300px";
-    }
+    winCondition();
+    lostCondtion();
 })
 
-setInterval (() => {
-    const moveX = Math.ceil(Math.random()*3);
-    const moveY = Math.ceil(Math.random()*3);
-    //math.random = 0->1 math.round forces 0|1 0 is falsy 1 is truthy. 3 cases
+// GHOST MOVE RANDOMLY
+// setInterval (() => {
+//     const moveX = Math.ceil(Math.random()*3);
+//     const moveY = Math.ceil(Math.random()*3);
+//     //math.random = 0->1 math.round forces 0|1 0 is falsy 1 is truthy. 3 cases
 
     
-    switch(moveX) {
-        case 1:
-            ghostPosition.x += 100;
-        case 2:
-            ghostPosition.x -= 100;
-            break;
-    }
+//     switch(moveX) {
+//         case 1:
+//             ghostPosition.x += 100;
+//         case 2:
+//             ghostPosition.x -= 100;
+//             break;
+//     }
     
-    switch(moveY){
-        case 1:
-            ghostPosition.y += 100;
-            break;
-        case 2:
-        ghostPosition.y -= 100;
-        break;
-    }
-    if (ghostPosition.x > 700) ghostPosition.x = 700;
-    if (ghostPosition.x < 0 ) ghostPosition.x = 0;
-    if (ghostPosition.y > 500) ghostPosition.y = 500;
-    if (ghostPosition.y < 0) ghostPosition.y = 0;
+//     switch(moveY){
+//         case 1:
+//             ghostPosition.y += 100;
+//             break;
+//         case 2:
+//         ghostPosition.y -= 100;
+//         break;
+//     }
+//     if (ghostPosition.x > 700) ghostPosition.x = 700;
+//     if (ghostPosition.x < 0 ) ghostPosition.x = 0;
+//     if (ghostPosition.y > 500) ghostPosition.y = 500;
+//     if (ghostPosition.y < 0) ghostPosition.y = 0;
     
-    ghost.style.left = ghostPosition.x+ "px";
-    ghost.style.top = ghostPosition.y + "px";
-}, 1000)
+//     ghost.style.left = ghostPosition.x+ "px";
+//     ghost.style.top = ghostPosition.y + "px";
+// }, 1000)
 
 
 
@@ -116,3 +140,38 @@ setInterval (() => {
 
 
 // The keydown and keyup events provide a code indicating which key is pressed, while keypress indicates which character was entered.
+
+
+
+// Logic to get the ghost follow hero
+setInterval (() => {
+    const moveX = Math.round(Math.random());
+    //math.random = 0->1 math.round forces 0|1 0 is falsy 1 is truthy. 3 cases
+    if (moveX) {
+        if (ghostPosition.x < heroPosition.x) {
+            ghostPosition.x += 100;
+        }
+    
+        if (ghostPosition.x > heroPosition.x) {
+            ghostPosition.x -= 100;
+        }
+    } else {
+    if (ghostPosition.y < heroPosition.y) {
+        ghostPosition.y += 100;
+    }
+
+    if (ghostPosition.y > heroPosition.y) {
+        ghostPosition.y -= 100;
+    }
+}
+    
+    if (ghostPosition.x > 700) ghostPosition.x = 700;
+    if (ghostPosition.x < 0 ) ghostPosition.x = 0;
+    if (ghostPosition.y > 500) ghostPosition.y = 500;
+    if (ghostPosition.y < 0) ghostPosition.y = 0;
+    
+    ghost.style.left = ghostPosition.x+ "px";
+    ghost.style.top = ghostPosition.y + "px";
+    winCondition();
+    lostCondtion();
+}, 1000)
