b7302e837ff15be56de185818102180d3bbc6174
Christie Pham <115462474+christie0902@users.noreply.github.com>
2024-01-25 21:08:32 +0100

    add win and lost screen

--- js/game.js
+++ js/game.js
@@ -12,7 +12,9 @@ treasure.style.top = '0px';
 treasure.style.left = '400px';
 
 const heroPosition = {x:100,y:500};
-
+const treasurePosition = {x:400, y:0};
+const ghostPosition = {x:600, y:200};
+const map = document.querySelector('.map');
 
 document.addEventListener("keydown", (event) => {
     switch (event.keyCode) {
@@ -41,12 +43,37 @@ document.addEventListener("keydown", (event) => {
     if (heroPosition.y > 500) heroPosition.y = 500;
     if (heroPosition.y < 0) heroPosition.y = 0;
 
-    
-
     hero.style.top = heroPosition.y + "px";
     hero.style.left = heroPosition.x + "px";
-    console.log("position: ", heroPosition.y, heroPosition.x);
+    
+    if (heroPosition.x === treasurePosition.x && heroPosition.y === treasurePosition.y) {
+        map.style.opacity="0.5";
+        const winMessage = document.createElement('h1');
+        map.appendChild(winMessage);
+        winMessage.innerText="YOU WIN!";
+        winMessage.style.color= "green";
+        winMessage.style.backgroundColor = "white";
+        winMessage.style.fontFamily = "sans-serif";
+        winMessage.style.position = "absolute";
+        winMessage.style.top= "260px";
+        winMessage.style.left= "300px";
+    }
+
+    if (heroPosition.x === ghostPosition.x && heroPosition.y === ghostPosition.y) {
+        map.style.opacity="0.5";
+        const lostMessage = document.createElement('h1');
+        map.appendChild(lostMessage);
+        lostMessage.innerText="YOU LOSE!";
+        lostMessage.style.color= "red";
+        lostMessage.style.backgroundColor = "white";
+        lostMessage.style.fontFamily = "sans-serif";
+        lostMessage.style.position = "absolute";
+        lostMessage.style.top= "260px";
+        lostMessage.style.left= "300px";
+    }
 })
+
+
 
 
 
